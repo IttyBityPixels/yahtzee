43241d4daef991121e23b77aa95f81e0ffec57a6
John Albright <jalbright10@gmail.com>
2012-10-22 15:58:31 -0400

    Rolls 5 dice and prints their values

---
+++ yahtzee.py
@@ -0,0 +1,13 @@
+import random
+
+dieOne = random.randint(1,6)
+dieTwo = random.randint(1,6)
+dieThree = random.randint(1,6)
+dieFour = random.randint(1,6)
+dieFive = random.randint(1,6)
+
+print dieOne
+print dieTwo
+print dieThree
+print dieFour
+print dieFive
