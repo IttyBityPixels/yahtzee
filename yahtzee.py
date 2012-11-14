79e004eaa446d13b1578e4a3e266d9599d47f0ac
John Albright <jalbright10@gmail.com>
2012-11-13 23:39:08 -0500

    Fixed prompt and added 3 roll limit

--- yahtzee.py
+++ yahtzee.py
@@ -2,18 +2,23 @@ import random
 import sys
 
 selection = ""
+rollsLeft = 3
 
 def setup_game():
     print "Let's play Yahtzee!"
-    print "Choices:"
-    print "roll - roll the dice"
-    print "quit - quit the game"
 
 def prompt():
     global selection
+    global rollsLeft
+
+    print "Choices:"
+    print "roll - roll the dice (You have " + str(rollsLeft) + " left)"
+    print "quit - quit the game"
     selection = raw_input('Selection: ')
 
 def roll_dice():
+    global rollsLeft
+
     dieOne = random.randint(1,6)
     dieTwo = random.randint(1,6)
     dieThree = random.randint(1,6)
@@ -26,13 +31,15 @@ def roll_dice():
     print dieFour
     print dieFive
 
+    rollsLeft -= 1
+
 setup_game()
 
 
 while True:
     prompt()
 
-    if selection == "roll":
+    if selection == "roll" and rollsLeft > 0:
         print "Rolling dice"
         roll_dice()
     elif selection == 'quit':
