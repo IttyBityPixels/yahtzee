9ce8d74cbdef0959492cf1e5aa0cfe4ee92fb5af
John Albright <jalbright10@gmail.com>
2012-11-13 23:10:18 -0500

    Basic selection prompt

--- yahtzee.py
+++ yahtzee.py
@@ -1,13 +1,40 @@
 import random
+import sys
 
-dieOne = random.randint(1,6)
-dieTwo = random.randint(1,6)
-dieThree = random.randint(1,6)
-dieFour = random.randint(1,6)
-dieFive = random.randint(1,6)
+selection = ""
 
-print dieOne
-print dieTwo
-print dieThree
-print dieFour
-print dieFive
+def setup_game():
+    print "Let's play Yahtzee!"
+    print "Choices:"
+    print "roll - roll the dice"
+    print "quit - quit the game"
+
+def prompt():
+    global selection
+    selection = raw_input('Selection: ')
+
+def roll_dice():
+    dieOne = random.randint(1,6)
+    dieTwo = random.randint(1,6)
+    dieThree = random.randint(1,6)
+    dieFour = random.randint(1,6)
+    dieFive = random.randint(1,6)
+
+    print dieOne
+    print dieTwo
+    print dieThree
+    print dieFour
+    print dieFive
+
+setup_game()
+
+
+while True:
+    prompt()
+
+    if selection == "roll":
+        print "Rolling dice"
+        roll_dice()
+    elif selection == 'quit':
+        print "Thanks for playing."
+        sys.exit(0)
