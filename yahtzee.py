21267a875129ecec2563bcf9987b16e698311191
John Albright <jalbright10@gmail.com>
2012-11-30 12:06:02 -0500

    Added ability to hold and release dice

--- yahtzee.py
+++ yahtzee.py
@@ -3,6 +3,8 @@ import sys
 
 selection = ""
 rollsLeft = 3
+rolls = [0, 0, 0, 0, 0]
+holds = [0, 0, 0, 0, 0]
 
 def setup_game():
     print "Let's play Yahtzee!"
@@ -13,25 +15,49 @@ def prompt():
 
     print "Choices:"
     print "(1) Roll - roll the dice (You have " + str(rollsLeft) + " left)"
-    print "(2) Quit - quit the game"
+    print "(2) Hold/Release - hold or release dice"
+    print "(3) Quit - quit the game"
     selection = raw_input('Selection: ')
 
 def roll_dice():
     global rollsLeft
+    global rolls
+    global holds
 
-    dieOne = random.randint(1,6)
-    dieTwo = random.randint(1,6)
-    dieThree = random.randint(1,6)
-    dieFour = random.randint(1,6)
-    dieFive = random.randint(1,6)
+    # dieOne = random.randint(1,6)
+    # dieTwo = random.randint(1,6)
+    # dieThree = random.randint(1,6)
+    # dieFour = random.randint(1,6)
+    # dieFive = random.randint(1,6)
 
-    print dieOne
-    print dieTwo
-    print dieThree
-    print dieFour
-    print dieFive
+    for i in range(5):
+        if holds[i] == 0:
+            rolls[i] = random.randint(1,6)
+
+    print rolls
 
     rollsLeft -= 1
+
+def hold_prompt():
+    print "HOLD STATUS"
+
+    for index, item in enumerate(holds):
+        if item == 0:
+            print str(index + 1) + ": released"
+        else:
+            print str(index + 1) + ": held"
+
+    print "Specify which dice you would like to hold or release"
+
+    dice = raw_input('> ').split(' ')
+
+    for i in dice:
+        index = int(i) - 1
+
+        if holds[index] == 0:
+            holds[index] = 1
+        else:
+            holds[index] = 0
 
 setup_game()
 
@@ -46,5 +72,7 @@ while True:
         else:
             print "You can not roll anymore"
     elif selection == '2':
+        hold_prompt()
+    elif selection == '3':
         print "Thanks for playing."
         sys.exit(0)
